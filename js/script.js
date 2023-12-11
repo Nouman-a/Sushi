5c850b39da32f90286e3fec2339d7a74b86cf236
Nouman Ahmed <Noumanahmedoffical@gmail.com>
2023-12-11 17:30:24 +0500

    deployed

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ import sushi12 from '../assets/sushi-12.png'
 import sushi11 from '../assets/sushi-11.png'
 import sushi10 from '../assets/sushi-10.png'
 
+import { injectSpeedInsights } from "@vercel/speed-insights"
+
 import AOS from "aos";
 import "aos/dist/aos.css";
 
